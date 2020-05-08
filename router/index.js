ffdfc2ebbcb3cfd357b052dcb7580b86c2f6f848
yuho <liu.yu.hao@qq.com>
2020-05-08 11:55:23 +0800

    2

--- router/index.js
+++ router/index.js
@@ -2,8 +2,6 @@ import Vue from 'vue';
 import Router from 'vue-router';
 // import Home from 'pages/home';
 // import Category from 'pages/category';
-// import Cart from 'pages/cart';
-// import Personal from 'pages/personal';
 // import Search from 'pages/search';
 // import Product from 'pages/product';
 
@@ -26,16 +24,6 @@ const routes = [
     name: 'category',
     path: '/category',
     component: () => import('pages/category')
-  },
-  {
-    name: 'cart',
-    path: '/cart',
-    component: () => import('pages/cart')
-  },
-  {
-    name: 'personal',
-    path: '/personal',
-    component: () => import('pages/personal')
   },
   {
     name: 'search',
